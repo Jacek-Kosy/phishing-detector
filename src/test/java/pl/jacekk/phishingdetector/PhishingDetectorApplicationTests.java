4e54ed9b876f770822d8264a3ce098bf9d229590
Jacek Kosy <jacek.kosy@outlook.de>
2023-09-01 22:17:21 +0200

    initial commit

---
+++ src/test/java/pl/jacekk/phishingdetector/PhishingDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacekk.phishingdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
