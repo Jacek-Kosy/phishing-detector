d6081785c5f5ca7781f78145ae5c19160b5e926b
Jacek Kosy <jacek.kosy@outlook.de>
2023-09-02 17:12:53 +0200

    added basic H2 database and Embedded Kafka config

--- src/main/java/pl/jacekk/phishingdetector/PhishingDetectorApplication.java
+++ src/main/java/pl/jacekk/phishingdetector/PhishingDetectorApplication.java
@@ -2,12 +2,21 @@ package pl.jacekk.phishingdetector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 
 @SpringBootApplication
 public class PhishingDetectorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhishingDetectorApplication.class, args);
+    }
+
+    @Bean
+    EmbeddedKafkaBroker broker() {
+        return new EmbeddedKafkaBroker(1)
+                .kafkaPorts(9092)
+                .brokerListProperty("spring.kafka.bootstrap-servers");
     }
 
 }
